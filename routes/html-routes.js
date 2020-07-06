3bb47c22f07f8cb39e3ceed68d8550b54141f0b4
Joel Clifford <joel.clifford@icloud.com>
2020-07-05 21:06:02 -0400

    added html page routes

---
+++ routes/html-routes.js
@@ -0,0 +1,21 @@
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
+  app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
+};
