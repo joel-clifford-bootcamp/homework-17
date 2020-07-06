7e2979448fcd3d9b8d9eed8691a8ec920fa929df
Joel Clifford <joel.clifford@icloud.com>
2020-07-05 22:37:37 -0400

    completed routes

---
+++ routes/api-routes.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+const { Workout, Exercise } = require("../models/index");
+
+module.exports = function(app) {
+
+  app.get("/api/workouts", ( req, res) => {
+    Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+  });
+
+  app.get("/api/workouts/range", ( req, res) => {
+    console.log("get workouts");
+    res.status(200).json({});
+  });
+
+  app.post("/api/workouts", ( { body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+  });
+
+  app.put("/api/workouts/:id", ( req, res) => {
+    var workoutId = req.params.id;
+
+    Exercise.create(req.body).then(exercise => {
+      Workout.update(
+        {_id: workoutId},
+        {$push: {exercises: exercise}}
+      )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      });
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+  });
+}
